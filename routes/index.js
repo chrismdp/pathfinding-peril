ad849cc2507c8159176a216f3dea104b4edf643d
Chris Parsons <chris@thinkcodelearn.com>
2016-06-22 15:09:28 +0100

    Basic map

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
 
+var players = [];
+var map = [];
+
+var template = [
+  "XXXXXXXXXXXXXXXXXXXX",
+  "X............X.....X",
+  "X...XX.......XFXX..X",
+  "X....XX..XXX.X.XXX.X",
+  "X.....X....X.X.....X",
+  "X.....X..XXX.XXX.X.X",
+  "X...XXX......X.....X",
+  "X...X...XXXX...XXX.X",
+  "X.....XXX..X.X.X...X",
+  "X.....X....X.......X",
+  "X........XXX..XXXXXX",
+  "X..................X",
+  "XXXXXXXXXXXXXXXXXXXX",
+];
+
+for (var i = 0; i < template.length; i++) {
+  var row = template[i];
+  for (var j = 0; j < row.length; j++) {
+    var tile = row[j];
+    if (tile == "X") {
+      map.push({ image: 'wall', x: j, y: i });
+    } else if (tile == ".") {
+      map.push({ image: 'floor', x: j, y: i });
+    } else if (tile == "F") {
+      map.push({ image: 'target', x: j, y: i });
+    }
+  }
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello, world!' });
+  res.render('index', { title: 'Pathfinding Peril', players: players, map: map });
 });
+
+router.get('/index.json', function(req, res, next) {
+  res.send({ players: players });
+});
+
+router.post('/players.json', function(req, res, next) {
+  players.push(req.body.name);
+  res.redirect('/index.json');
+});
+
 
 module.exports = router;
--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,23 @@
+
 module.exports = {
-  'Demo test' : function (browser) {
+  'Viewing the map' : function (browser) {
     browser
       .url('http://localhost:3000/')
       .waitForElementVisible('body', 1000)
-      .assert.containsText('body', 'Hello, world!')
-      .end();
-  }
+      .assert.elementPresent('#map')
+  },
+  'Adding a player' : function (browser) {
+    var request = require('request');
+    request({
+      url: "http://localhost:3000/players.json",
+      method: "POST",
+      json: {"name": "Player One"},
+    });
+
+    browser
+      .url('http://localhost:3000/')
+      .waitForElementVisible('body', 1000)
+      .assert.containsText('body', 'Pathfinding Peril')
+      .assert.containsText('#players', 'Player One');
+  },
 };
